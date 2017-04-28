68dfeabee26a1498ce6a70e30f2099fe79519d47
justincasali <justin.casali@colorado.edu>
2017-04-28 17:47:48 -0600

    fixed duplicate code

--- pipeFuncs.h
+++ pipeFuncs.h
@@ -142,34 +142,6 @@ void instruction_decode(IFID_Reg *IFID,IDEX_Reg *IDEX){
 void execute(IDEX_Reg *IDEX,EXMEM_Reg *EXMEM,MEMWB_Reg *MEMWB){
 
   // Forwarding
-  // EX Hazards
-  // R-type to R-type
-  if((EXMEM->RegWrite) && (EXMEM->Rd != 0) && (IDEX->Rd != 0) && (EXMEM->Rd == IDEX->Rs)){
-    //ForwardA = 10
-    IDEX->readRs = EXMEM->aluResult;
-  }
-  if((EXMEM->RegWrite) && (EXMEM->Rd != 0) && (IDEX->Rd != 0) && (EXMEM->Rd == IDEX->Rt)){
-    //ForwardB = 10
-    IDEX->readRt = EXMEM->aluResult;
-  }
-  // I-Type to I-type
-  if((EXMEM->RegWrite) && (EXMEM->Rd == 0) && (IDEX->Rd == 0) && (EXMEM->Rt == IDEX->Rs)){
-    //ForwardA = 10
-    IDEX->readRs = EXMEM->aluResult;
-  }
-  // I-Type to R-Type
-  if((EXMEM->RegWrite) && (EXMEM->Rd == 0) && (IDEX->Rd != 0) && (EXMEM->Rt == IDEX->Rs)){
-    IDEX->readRs = EXMEM->aluResult;
-  }
-  if((EXMEM->RegWrite) && (EXMEM->Rd == 0) && (IDEX->Rd != 0) && (EXMEM->Rt == IDEX->Rt)){
-    IDEX->readRt = EXMEM->aluResult;
-  }
-  // R-Type to I-Type
-  if((EXMEM->RegWrite) && (EXMEM->Rd != 0) && (IDEX->Rd == 0) && (EXMEM->Rd == IDEX->Rs)){
-    IDEX->readRs = EXMEM->aluResult;
-  }
-
-  // Forwarding
   // MEM Hazards
   // R-Type
   if((MEMWB->RegWrite) && (MEMWB->Rd != 0) && (MEMWB->Rd == IDEX->Rs)){
