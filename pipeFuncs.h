e6a424f6cdaa891474fa188142b4be46dc37799f
justincasali <justin.casali@colorado.edu>
2017-05-01 12:49:55 -0600

    Fixed jump address

--- pipeFuncs.h
+++ pipeFuncs.h
@@ -228,7 +228,7 @@ void instruction_decode(IFID_Reg *IFID,IDEX_Reg *IDEX,EXMEM_Reg *EXMEM){
   }
 
   // Calculating branch address
-  IDEX->branchPC = IFID->PCplus1+(IFID->immediate>>2);
+  IDEX->branchPC = IFID->PCplus1 + (int32_t)IFID->immediate;
 
   switch (IFID->Opcode){
     // beq
