bd0c5afa52213ef48f091ee2e1cf2c2e5301f71b
charlieKing4967 <charlie_king4967@hotmail.com>
2017-05-01 15:47:52 -0600

    Added (very basic) i-Cache

--- cacheFunctions.h
+++ cacheFunctions.h
@@ -1,1 +1,20 @@
 #include "regDefs.h"
+
+bool dValid[8];
+int dTag[8];
+int dData[8];
+
+int programMemoryRead(){
+    int index = pc & 0x7;
+    int tag = pc >> 3;
+    if((dTag[index] == tag) && (dValid[index]){
+        // cache hit
+
+    }
+    else{
+        // cache miss
+        dData[index] = programMemory[pc];
+        dValid[index] = 1;
+    }
+    return dData[index];
+}
