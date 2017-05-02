a4bebbfe06aa26db573be13f662dfca9f10941d9
charlieKing4967 <charlie_king4967@hotmail.com>
2017-05-01 19:10:38 -0600

    I-Cache is working with variable sizes

--- cacheFunctions.cpp
+++ cacheFunctions.cpp
@@ -1,50 +1,62 @@
 #include "regDefs.h"
 #include<iostream>
 #include<string>
+#include<cmath>
 
 using namespace std;
 
-bool dValid[8];
-int dTag[8];
-int dData[8];
+#define iCacheSize 8
+#define dCacheSize 8
+#define blockSize 4
+
+bool iValid[iCacheSize];
+int iTag[iCacheSize];
+int iData[iCacheSize][blockSize];
 
 int programMemoryRead(){
-    int index = pc & 0x7;
-    int tag = pc >> 3;
-    if((dTag[index] == tag) && (dValid[index])){
+    int blockOffset = pc & (blockSize-1);
+    int index = (((iCacheSize-1)*blockSize)&pc)/blockSize;
+    int tag = pc / (blockSize*iCacheSize);
+    if((iTag[index] == tag) && (iValid[index])){
         // cache hit
         cout << "Cache Hit\n";
     }
     else{
         // cache miss
-        dData[index] = programMemory[pc];
-        dValid[index] = 1;
-        dTag[index] = tag;
+        for(int i = 0; i <= (blockSize-1); i++){
+            iData[index][i] = programMemory[(pc & ~(blockSize-1)) + i];
+        }
+        iValid[index] = 1;
+        iTag[index] = tag;
         cout << "Cache Miss\n";
     }
-    return dData[index];
+    return iData[index][blockOffset];
 }
 
 int main(){
     programMemory[0] = 5;
     programMemory[1] = 10;
     programMemory[2] = 15;
-    programMemory[9] = 20;
+    programMemory[3] = 20;
+    programMemory[4] = 50;
     int result;
+    pc = 5;
+    result = programMemoryRead();
+    cout << result << "\n";
+    pc = 4;
     result = programMemoryRead();
     cout << result << "\n";
     pc = 1;
     result = programMemoryRead();
     cout << result << "\n";
-    pc = 2;
+    pc = 3;
     result = programMemoryRead();
     cout << result << "\n";
-    pc = 9;
+    pc = 4;
     result = programMemoryRead();
     cout << result << "\n";
-    pc = 1;
+    pc = 8;
     result = programMemoryRead();
     cout << result << "\n";
-
 
 }
